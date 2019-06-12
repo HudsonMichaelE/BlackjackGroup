6697fc2445b57b1558001284cd52d435e571bcd4
HudsonMichaelE <hudsonmichaele@outlook.com>
2019-06-12 18:41:54 -0400

    Started working on engine

--- Blackjack/src/Blackjack/BlackjackSingleton.java
+++ Blackjack/src/Blackjack/BlackjackSingleton.java
@@ -1,6 +1,38 @@
 package Blackjack;
 
+import java.util.*;
+
 //Holds the engine of the game
 public class BlackjackSingleton {
-
+	private Dealer dealer;
+	private ArrayList<Player> players;
+	private int playerNum = 0;
+	
+	private static BlackjackSingleton instance;
+	
+	
+	
+	private BlackjackSingleton() {
+		dealer = new Dealer();
+	}
+	
+	public BlackjackSingleton getInstance() {
+		if(instance == null) {
+			instance = new BlackjackSingleton();
+		}
+		
+		return instance;
+	}
+	
+	public void addPlayer(Player newPlayer) {
+		players.add(newPlayer);
+		playerNum += 1;
+	}
+	
+	public Dealer getDealer() {
+		return dealer;
+	}
+	/********************************************/
+	
+	
 }
--- Blackjack/src/Blackjack/Player.java
+++ Blackjack/src/Blackjack/Player.java
@@ -2,4 +2,13 @@ package Blackjack;
 
 public interface Player {
 
+	int placeBet();
+	
+	Boolean doesPlayerHit();
+	
+	void Hit(char Card);
+	
+	int getScore();
+	
+	
 }
